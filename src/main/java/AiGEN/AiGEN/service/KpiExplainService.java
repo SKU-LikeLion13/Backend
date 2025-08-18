3c7cc3cdc90369febadb0e690db5793539ec4a22
hong0678 <hong0678@naver.com>
2025-08-18 15:31:09 +0900

    add_service_

--- src/main/java/AiGEN/AiGEN/service/KpiExplainService.java
+++ src/main/java/AiGEN/AiGEN/service/KpiExplainService.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
